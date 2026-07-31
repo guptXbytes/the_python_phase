465abf240abb1acd8ddbf3dc130a963e80fac923
guptXbytes <officiallysumit432@gmail.com>
2026-07-31 12:56:56 +0530

    Second python mini project!

---
+++ projects/number_guessing/Readme.md
@@ -0,0 +1,24 @@
+# number_guessing 
+
+#what does it do?
+It generates a random number and lets the user guess it till the correct guess is found.
+
+#what are its features?
+1.It asks the user for the maximum number.
+2.If the number is not entered, the user is exited.
+3.It generates the random number.
+4.It lets the user guess till the correct guess is found.
+5.Every guess attempt is counted
+6.It generates in how many attempts the guess was correct.
+
+#what technologies did i use?
+1.imported the random module.
+2.if-else statements
+3.while loop
+4.print for printing the attempt count.
+
+#how do i run it?
+python number_guessing.py
+
+#who made it?
+Sumith Kumar Gupta
--- projects/number_guessing/number_guessing.py
+++ projects/number_guessing/number_guessing.py
@@ -1,2 +1,32 @@
 import random
-random.randrange(-1,10)
+
+higher_limit = input("Enter the number: ")
+
+if higher_limit.isdigit():
+    higher_limit = int(higher_limit)
+    if higher_limit <=0:
+        print("Enter a number greater than 0.")
+        quit()
+else:
+    print("Try entering a number next time.")
+    quit()        
+
+random_number = random.randint(0,higher_limit)
+guesses = 0 
+while True:
+    guesses +=1
+    guess_number = input("Make a guess: ")
+    if guess_number.isdigit():
+        guess_number = int(guess_number)
+    else:
+        print("Try again by entering a number.")
+        continue    
+    if guess_number == random_number:
+        print("You got it")
+        break
+    elif guess_number > random_number:
+        print("You are higher the number")
+    else:
+        print("You are below the number")
+    
+print("You made it in",guesses,"guesses")        
