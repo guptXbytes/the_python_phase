2e3837fdfb7f60c2d3f7c17f0cf28c3b0f747978
guptXbytes <officiallysumit432@gmail.com>
2026-08-02 00:37:32 +0530

    Updated the project rock_paper_scissors

--- projects/rock_paper_scissors/Readme.md
+++ projects/rock_paper_scissors/Readme.md
@@ -1,4 +1,22 @@
 # rock_paper_scissors game
 
 #Description.
-This project is the display of the game rock, paper , scissors till the user wants to play the game where the computer is taking random guess among the three and number of wins is calculated for both the players.
+This project lets the user play the rock, paper , scissors game with the computer.
+
+#Features.
+1.The user has the option to play the game till he needs to quit.
+2.The computer takes the option by using the random module.
+3.If the input entered is out of options , it is redirected to again ask the input.
+4.The comp_option and user_option are being displayed.
+5.The program goes with the game rules and give the winner/ loser/ draw everytime the input is entered.
+6.When the user exits the game, the total wins by both comp and user is displayed.
+7.The final winner is displayed.
+
+#techstack 
+python
+
+#how to run
+python rock_paper_scissors.py
+
+#author
+Sumith Kumar Gupta
--- projects/rock_paper_scissors/rock_paper_scissors.py
+++ projects/rock_paper_scissors/rock_paper_scissors.py
@@ -6,18 +6,42 @@ options = ["rock" , "paper" , "scissors"]
 
 while True:
     user_option = input("Choose 1.rock, 2.paper, 3.scissors and 4.q for exit: ").lower()
-    comp_option = random.randint(0,2)
+    print("The user selected: ",user_option + ".")
+
+    
     if user_option == "q":
         print("Exiting the game...")
         break
-    elif user_option in options:
-        if user_option == "rock" and options[comp_option] == "scissors" :
-            print("The user won the game!")
-            user_wins +=1
-        elif user_option == "scissors" and  options[comp_option] == "paper":
-            print("The user won the game!")
-            user_wins +=1
-        elif user_option == "paper" and options[comp_option] == "rock":
-            print("The user won the game!")
-            user_wins +=1   
+    if user_option not in options:
+        print("Select a valid option!")
+        continue
+    comp_option = random.randint(0,2)
+    print("The comp selected: ", options[comp_option] + ".")
+    
+    if user_option == "rock" and options[comp_option] == "scissors" :
+        print("The user won the game!")
+        user_wins +=1
+    elif user_option == "scissors" and  options[comp_option] == "paper":
+        print("The user won the game!")
+        user_wins +=1
+    elif user_option == "paper" and options[comp_option] == "rock":
+        print("The user won the game!")
+        user_wins +=1
+    elif user_option == "rock" and options[comp_option] == "rock":
+        print("It has been draw...") 
+    elif user_option == "scissors" and options[comp_option] == "scissors":
+            print("It has been draw...") 
+    elif user_option == "paper" and options[comp_option] == "paper":
+            print("It has been draw...")        
+    else:
+         print("You lost!!")
+         comp_wins +=1                  
             
+print("The number of user wins: ",user_wins)
+print("The number of comp wins: ",comp_wins)
+if user_wins > comp_wins:
+     print("User is the WINNER!")
+elif user_wins< comp_wins:
+     print("Comp is the WINNER!")
+else :
+     print("It is a TIE!!")          
